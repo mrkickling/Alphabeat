7d062b3ebf0b66f974e59256adf0508c4961cf49
Joakim Loxdal <joakim.loxdal@ibm.com>
2016-11-18 09:26:58 +0100

    Updated read me added mysql module

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
-# Node.js Starter Overview
+# Alphabeat the game
+A game where the users compete against each other to write down the alphabet fastest.
 
-The Node.js Starter demonstrates a simple, reusable Node.js web application based on the Express framework.
-
-## Run the app locally
-
-1. [Install Node.js][]
-2. Download and extract the starter code from the Bluemix UI
-3. cd into the app directory
-4. Run `npm install` to install the app's dependencies
-5. Run `npm start` to start the app
-6. Access the running app in a browser at http://localhost:6001
-
-[Install Node.js]: https://nodejs.org/en/download/
+## About the app
+Made with nodejs, angularjs and is connected to a mysql server.
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /*eslint-env node*/
+var Credentials = require('./credentials');
+var credentials = Credentials.get();
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
@@ -7,6 +9,27 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : credentials.mysqlhost,
+  user     : credentials.mysqluser,
+  password : credentials.mysqlpassword,
+  database : credentials.mysqldb
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM alphabeat', function(err, rows, fields) {
+  if (err){
+      console.log(err)
+  }
+      
+ 
+  console.log('The solution is: ', rows[0].solution);
+});
+ 
+connection.end();
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
